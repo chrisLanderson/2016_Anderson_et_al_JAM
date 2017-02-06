99b0f177eabd4546aba5d2a4262ac2fa0ff457ec
Chris Anderson <canderson30@unl.edu>
2017-02-06 12:22:37 -0600

    through shared otus

--- functions.R
+++ functions.R
@@ -18,3 +18,35 @@ multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
     }
   }
 }
+
+collapse_treatment <- function(x, y) {
+  order_vec <- sort(c(x[[1]], y[[1]]))
+  collapse_vec <- str_c(order_vec[1], order_vec[2], sep = "-")
+}
+
+max_shared_otu <- function(x, y) {
+  max_x_y <- max(share_otu[x], share_otu[y])
+}
+
+seq_shared <- function(x) {
+  sub_tables <- collapse_tidy %>% 
+    filter(sample %in% x)
+  
+  join_tables <- inner_join(sub_tables$data[[1]], sub_tables$data[[2]], by = "otu") %>% 
+    mutate(seqs = abundance.x + abundance.y)
+  
+  shared_seqs <- sum(join_tables$seqs)
+  
+  seq1 <- sum(sub_tables$data[[1]]$abundance)
+  
+  seq2 <- sum(sub_tables$data[[2]]$abundance)
+  
+  total_seqs <- seq1 + seq2
+  
+  per_seqs <-  (shared_seqs / total_seqs) * 100                              
+  
+  out <- str_c(x[[1]], x[[2]], shared_seqs, total_seqs, per_seqs, sep = "\t")
+  
+  write(out, file = "TableS3_.txt", sep = "\t", ncolumns = 5, append = TRUE)
+}
+
