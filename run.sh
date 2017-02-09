f3c0299becb62e681a4eeefb65a2612a9fd3c107
Chris Anderson <canderson30@unl.edu>
2017-02-09 09:17:49 -0600

    change conda install

--- run.sh
+++ run.sh
@@ -12,8 +12,8 @@ bash Miniconda2-4.2.12-MacOSX-x86_64.sh -b -p anaconda
 
 # anaconda/bin/conda list -n qiime --export > conda_qiime_packages.txt
 # anaconda/bin/conda list -n r --export > conda_r_packages.txt
-anaconda/bin/conda create -y -n qiime --file conda_qiime_packages.txt
-anaconda/bin/conda create -y -n r --file conda_r_packages.txt
+anaconda/bin/conda create -y -n qiime --file conda_qiime_packages.txt -c bioconda -c r -c biobuilds
+anaconda/bin/conda create -y -n r --file conda_r_packages.txt -c bioconda -c r -c biobuilds
 source anaconda/bin/activate qiime
 
 wget -O anaconda/envs/qiime/bin/usearch $1
